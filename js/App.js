64b0f2d2a97a5c500f80b257ad5f6ad149e80715
Nick GS <gsidnick@gmail.com>
2023-04-23 09:40:11 +0300

    feat: render components on page

--- js/App.js
+++ js/App.js
@@ -1,6 +1,8 @@
 import WrapperComponent from './components/WrapperComponent.js';
 import HeaderComponent from './components/HeaderComponent.js';
 import TextboxComponent from './components/TextboxComponent.js';
+import MainComponent from './components/MainComponent.js';
+import KeyboardComponent from './components/KeyboardComponent.js';
 
 class App {
   constructor() {
@@ -10,12 +12,11 @@ class App {
   start() {
     const wrapper = new WrapperComponent();
     const header = new HeaderComponent();
+    const main = new MainComponent();
     const textbox = new TextboxComponent();
-    wrapper.append(header);
-    wrapper.append(textbox);
-    if (this.body !== null) {
-      this.body.append(wrapper.node);
-    }
+    const keyboard = new KeyboardComponent();
+    wrapper.append(header, main.append(textbox, keyboard));
+    if (this.body !== null) this.body.append(wrapper.node);
   }
 }
 
