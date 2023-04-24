2faea74d36890a2bed60954c0a55e7c67c89137b
Nick GS <gsidnick@gmail.com>
2023-04-25 00:23:41 +0300

    feat: implement set textbox value

--- js/components/TextboxComponent.js
+++ js/components/TextboxComponent.js
@@ -3,6 +3,12 @@ import Component from './Component.js';
 class TextboxComponent extends Component {
   constructor() {
     super('textarea', 'textbox__field');
+    this.value = '';
+  }
+
+  setValue(value) {
+    this.value += value;
+    this.node.innerHTML = this.value;
   }
 }
 
