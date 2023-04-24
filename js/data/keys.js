ecfac09417ebf91de097b9a76e6bcaa9a057f7ea
Nick GS <gsidnick@gmail.com>
2023-04-24 15:06:31 +0300

    refactor: change keys structure

--- js/data/keys.js
+++ js/data/keys.js
@@ -1,107 +1,782 @@
-export const keysEn = {
-  alphabet: 'abcdefghijklmnopqrstuvwxyz',
+const keys = {
   row1: [
-    ['~', '`'],
-    ['!', '1'],
-    ['@', '2'],
-    ['#', '3'],
-    ['$', '4'],
-    ['%', '5'],
-    ['^', '6'],
-    ['&', '7'],
+    {
+      en: {
+        keyShift: '~',
+        keyBase: '`',
+      },
+      ru: {
+        keyShift: 'Ё',
+        keyBase: 'ё',
+      },
+      keyCode: 192,
+      code: 'Backquote',
+    },
+    {
+      en: {
+        keyShift: '!',
+        keyBase: '1',
+      },
+      ru: {
+        keyShift: '!',
+        keyBase: '1',
+      },
+      keyCode: 49,
+      code: 'Digit1',
+    },
+    {
+      en: {
+        keyShift: '@',
+        keyBase: '2',
+      },
+      ru: {
+        keyShift: '"',
+        keyBase: '2',
+      },
+      keyCode: 50,
+      code: 'Digit2',
+    },
+    {
+      en: {
+        keyShift: '#',
+        keyBase: '3',
+      },
+      ru: {
+        keyShift: '№',
+        keyBase: '3',
+      },
+      keyCode: 51,
+      code: 'Digit3',
+    },
+    {
+      en: {
+        keyShift: '$',
+        keyBase: '4',
+      },
+      ru: {
+        keyShift: ';',
+        keyBase: '4',
+      },
+      keyCode: 52,
+      code: 'Digit4',
+    },
+    {
+      en: {
+        keyShift: '%',
+        keyBase: '5',
+      },
+      ru: {
+        keyShift: '%',
+        keyBase: '5',
+      },
+      keyCode: 53,
+      code: 'Digit5',
+    },
+    {
+      en: {
+        keyShift: '^',
+        keyBase: '6',
+      },
+      ru: {
+        keyShift: ':',
+        keyBase: '6',
+      },
+      keyCode: 54,
+      code: 'Digit6',
+    },
+    {
+      en: {
+        keyShift: '&',
+        keyBase: '7',
+      },
+      ru: {
+        keyShift: '?',
+        keyBase: '7',
+      },
+      keyCode: 55,
+      code: 'Digit7',
+    },
+    {
+      en: {
+        keyShift: '*',
+        keyBase: '8',
+      },
+      ru: {
+        keyShift: '*',
+        keyBase: '8',
+      },
+      keyCode: 56,
+      code: 'Digit8',
+    },
+    {
+      en: {
+        keyShift: '(',
+        keyBase: '9',
+      },
+      ru: {
+        keyShift: '(',
+        keyBase: '9',
+      },
+      keyCode: 57,
+      code: 'Digit9',
+    },
+    {
+      en: {
+        keyShift: ')',
+        keyBase: '0',
+      },
+      ru: {
+        keyShift: ')',
+        keyBase: '0',
+      },
+      keyCode: 48,
+      code: 'Digit0',
+    },
+    {
+      en: {
+        keyShift: '_',
+        keyBase: '-',
+      },
+      ru: {
+        keyShift: '_',
+        keyBase: '-',
+      },
+      keyCode: 189,
+      code: 'Minus',
+    },
+    {
+      en: {
+        keyShift: '+',
+        keyBase: '=',
+      },
+      ru: {
+        keyShift: '+',
+        keyBase: '=',
+      },
+      keyCode: 187,
+      code: 'Equal',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Backspace',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Backspace',
+      },
+      keyCode: 8,
+      code: 'Backspace',
+    },
   ],
   row2: [
-    ['Q', 'q'],
-    ['W', 'w'],
-    ['E', 'e'],
-    ['R', 'r'],
-    ['T', 't'],
-    ['Y', 'y'],
-    ['U', 'u'],
-    ['I', 'i'],
-    ['O', 'o'],
-    ['P', 'p'],
-    ['{', '['],
-    ['}', ']'],
-    ['|', '\\'],
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Tab',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Tab',
+      },
+      keyCode: 9,
+      code: 'Tab',
+    },
+    {
+      en: {
+        keyShift: 'Q',
+        keyBase: 'q',
+      },
+      ru: {
+        keyShift: 'Й',
+        keyBase: 'й',
+      },
+      keyCode: 81,
+      code: 'KeyQ',
+    },
+    {
+      en: {
+        keyShift: 'W',
+        keyBase: 'w',
+      },
+      ru: {
+        keyShift: 'Ц',
+        keyBase: 'ц',
+      },
+      keyCode: 87,
+      code: 'KeyW',
+    },
+    {
+      en: {
+        keyShift: 'E',
+        keyBase: 'e',
+      },
+      ru: {
+        keyShift: 'У',
+        keyBase: 'у',
+      },
+      keyCode: 69,
+      code: 'KeyE',
+    },
+    {
+      en: {
+        keyShift: 'R',
+        keyBase: 'r',
+      },
+      ru: {
+        keyShift: 'К',
+        keyBase: 'к',
+      },
+      keyCode: 82,
+      code: 'KeyR',
+    },
+    {
+      en: {
+        keyShift: 'T',
+        keyBase: 't',
+      },
+      ru: {
+        keyShift: 'Е',
+        keyBase: 'е',
+      },
+      keyCode: 84,
+      code: 'KeyT',
+    },
+    {
+      en: {
+        keyShift: 'Y',
+        keyBase: 'y',
+      },
+      ru: {
+        keyShift: 'Н',
+        keyBase: 'н',
+      },
+      keyCode: 89,
+      code: 'KeyY',
+    },
+    {
+      en: {
+        keyShift: 'U',
+        keyBase: 'u',
+      },
+      ru: {
+        keyShift: 'Г',
+        keyBase: 'г',
+      },
+      keyCode: 85,
+      code: 'KeyU',
+    },
+    {
+      en: {
+        keyShift: 'I',
+        keyBase: 'i',
+      },
+      ru: {
+        keyShift: 'Ш',
+        keyBase: 'ш',
+      },
+      keyCode: 73,
+      code: 'KeyI',
+    },
+    {
+      en: {
+        keyShift: 'O',
+        keyBase: 'o',
+      },
+      ru: {
+        keyShift: 'Щ',
+        keyBase: 'щ',
+      },
+      keyCode: 79,
+      code: 'KeyO',
+    },
+    {
+      en: {
+        keyShift: 'P',
+        keyBase: 'p',
+      },
+      ru: {
+        keyShift: 'З',
+        keyBase: 'з',
+      },
+      keyCode: 80,
+      code: 'KeyP',
+    },
+    {
+      en: {
+        keyShift: '{',
+        keyBase: '[',
+      },
+      ru: {
+        keyShift: 'Х',
+        keyBase: 'х',
+      },
+      keyCode: 219,
+      code: 'BracketLeft',
+    },
+    {
+      en: {
+        keyShift: '}',
+        keyBase: ']',
+      },
+      ru: {
+        keyShift: 'Ъ',
+        keyBase: 'ъ',
+      },
+      keyCode: 221,
+      code: 'BracketRight',
+    },
+    {
+      en: {
+        keyShift: '|',
+        keyBase: '\\',
+      },
+      ru: {
+        keyShift: '/',
+        keyBase: '\\',
+      },
+      keyCode: 220,
+      code: 'Backslash',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Del',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Del',
+      },
+      keyCode: 46,
+      code: 'Delete',
+    },
   ],
   row3: [
-    ['A', 'a'],
-    ['S', 's'],
-    ['D', 'd'],
-    ['F', 'f'],
-    ['G', 'g'],
-    ['H', 'h'],
-    ['J', 'j'],
-    ['K', 'k'],
-    ['L', 'l'],
-    [':', ';'],
-    ['"', '\''],
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Caps Lock',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Caps Lock',
+      },
+      keyCode: 20,
+      code: 'CapsLock',
+    },
+    {
+      en: {
+        keyShift: 'A',
+        keyBase: 'a',
+      },
+      ru: {
+        keyShift: 'Ф',
+        keyBase: 'ф',
+      },
+      keyCode: 65,
+      code: 'KeyA',
+    },
+    {
+      en: {
+        keyShift: 'S',
+        keyBase: 's',
+      },
+      ru: {
+        keyShift: 'Ы',
+        keyBase: 'ы',
+      },
+      keyCode: 83,
+      code: 'KeyS',
+    },
+    {
+      en: {
+        keyShift: 'D',
+        keyBase: 'd',
+      },
+      ru: {
+        keyShift: 'В',
+        keyBase: 'в',
+      },
+      keyCode: 68,
+      code: 'KeyD',
+    },
+    {
+      en: {
+        keyShift: 'F',
+        keyBase: 'f',
+      },
+      ru: {
+        keyShift: 'А',
+        keyBase: 'а',
+      },
+      keyCode: 70,
+      code: 'KeyF',
+    },
+    {
+      en: {
+        keyShift: 'G',
+        keyBase: 'g',
+      },
+      ru: {
+        keyShift: 'П',
+        keyBase: 'п',
+      },
+      keyCode: 71,
+      code: 'KeyG',
+    },
+    {
+      en: {
+        keyShift: 'H',
+        keyBase: 'h',
+      },
+      ru: {
+        keyShift: 'Р',
+        keyBase: 'р',
+      },
+      keyCode: 72,
+      code: 'KeyH',
+    },
+    {
+      en: {
+        keyShift: 'J',
+        keyBase: 'j',
+      },
+      ru: {
+        keyShift: 'О',
+        keyBase: 'о',
+      },
+      keyCode: 74,
+      code: 'KeyJ',
+    },
+    {
+      en: {
+        keyShift: 'K',
+        keyBase: 'k',
+      },
+      ru: {
+        keyShift: 'Л',
+        keyBase: 'л',
+      },
+      keyCode: 75,
+      code: 'KeyK',
+    },
+    {
+      en: {
+        keyShift: 'L',
+        keyBase: 'l',
+      },
+      ru: {
+        keyShift: 'Д',
+        keyBase: 'д',
+      },
+      keyCode: 76,
+      code: 'KeyL',
+    },
+    {
+      en: {
+        keyShift: ':',
+        keyBase: ';',
+      },
+      ru: {
+        keyShift: 'Ж',
+        keyBase: 'ж',
+      },
+      keyCode: 186,
+      code: 'Semicolon',
+    },
+    {
+      en: {
+        keyShift: '"',
+        keyBase: '\'',
+      },
+      ru: {
+        keyShift: 'Э',
+        keyBase: 'э',
+      },
+      keyCode: 222,
+      code: 'Quote',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Enter',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Enter',
+      },
+      keyCode: 13,
+      code: 'Enter',
+    },
   ],
   row4: [
-    ['Z', 'z'],
-    ['X', 'x'],
-    ['C', 'c'],
-    ['V', 'v'],
-    ['B', 'b'],
-    ['N', 'n'],
-    ['M', 'm'],
-    ['<', ','],
-    ['>', '.'],
-    ['?', '/'],
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Shift',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Shift',
+      },
+      keyCode: 16,
+      code: 'ShiftLeft',
+    },
+    {
+      en: {
+        keyShift: 'Z',
+        keyBase: 'z',
+      },
+      ru: {
+        keyShift: 'Я',
+        keyBase: 'я',
+      },
+      keyCode: 90,
+      code: 'KeyZ',
+    },
+    {
+      en: {
+        keyShift: 'X',
+        keyBase: 'x',
+      },
+      ru: {
+        keyShift: 'Ч',
+        keyBase: 'ч',
+      },
+      keyCode: 88,
+      code: 'KeyX',
+    },
+    {
+      en: {
+        keyShift: 'C',
+        keyBase: 'c',
+      },
+      ru: {
+        keyShift: 'С',
+        keyBase: 'с',
+      },
+      keyCode: 67,
+      code: 'KeyC',
+    },
+    {
+      en: {
+        keyShift: 'V',
+        keyBase: 'v',
+      },
+      ru: {
+        keyShift: 'М',
+        keyBase: 'м',
+      },
+      keyCode: 86,
+      code: 'KeyV',
+    },
+    {
+      en: {
+        keyShift: 'B',
+        keyBase: 'b',
+      },
+      ru: {
+        keyShift: 'И',
+        keyBase: 'и',
+      },
+      keyCode: 66,
+      code: 'KeyB',
+    },
+    {
+      en: {
+        keyShift: 'N',
+        keyBase: 'n',
+      },
+      ru: {
+        keyShift: 'Т',
+        keyBase: 'т',
+      },
+      keyCode: 78,
+      code: 'KeyN',
+    },
+    {
+      en: {
+        keyShift: 'M',
+        keyBase: 'm',
+      },
+      ru: {
+        keyShift: 'Ь',
+        keyBase: 'ь',
+      },
+      keyCode: 77,
+      code: 'KeyM',
+    },
+    {
+      en: {
+        keyShift: '<',
+        keyBase: ',',
+      },
+      ru: {
+        keyShift: 'Б',
+        keyBase: 'б',
+      },
+      keyCode: 188,
+      code: 'Comma',
+    },
+    {
+      en: {
+        keyShift: '>',
+        keyBase: '.',
+      },
+      ru: {
+        keyShift: 'Ю',
+        keyBase: 'ю',
+      },
+      keyCode: 190,
+      code: 'Period',
+    },
+    {
+      en: {
+        keyShift: '?',
+        keyBase: '/',
+      },
+      ru: {
+        keyShift: ',',
+        keyBase: '.',
+      },
+      keyCode: 191,
+      code: 'Slash',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Shift',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Shift',
+      },
+      keyCode: 16,
+      code: 'ShiftRight',
+    },
+  ],
+  row5: [
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Ctrl',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Ctrl',
+      },
+      keyCode: 17,
+      code: 'ControlLeft',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Win',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Win',
+      },
+      keyCode: 91,
+      code: 'Meta',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Alt',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Alt',
+      },
+      keyCode: 18,
+      code: 'AltLeft',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: '',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: '',
+      },
+      keyCode: 32,
+      code: 'Space',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Alt',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Alt',
+      },
+      keyCode: 18,
+      code: 'AltRight',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: 'Ctrl',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: 'Ctrl',
+      },
+      keyCode: 17,
+      code: 'CtrlRight',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: '◄',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: '◄',
+      },
+      keyCode: 37,
+      code: 'ArrowLeft',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: '▲',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: '▲',
+      },
+      keyCode: 38,
+      code: 'ArrowUp',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: '▼',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: '▼',
+      },
+      keyCode: 40,
+      code: 'ArrowDown',
+    },
+    {
+      en: {
+        keyShift: null,
+        keyBase: '►',
+      },
+      ru: {
+        keyShift: null,
+        keyBase: '►',
+      },
+      keyCode: 39,
+      code: 'ArrowRight',
+    },
   ],
 };
 
-export const keysRu = {
-  alphabet: 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
-  row1: [
-    ['Ё', 'ё'],
-    ['!', '1'],
-    ['"', '2'],
-    ['№', '3'],
-    [';', '4'],
-    ['%', '5'],
-    [':', '6'],
-    ['?', '7'],
-  ],
-  row2: [
-    ['Й', 'й'],
-    ['Ц', 'ц'],
-    ['У', 'у'],
-    ['К', 'к'],
-    ['Е', 'е'],
-    ['Н', 'н'],
-    ['Г', 'г'],
-    ['Ш', 'ш'],
-    ['Щ', 'щ'],
-    ['З', 'з'],
-    ['Х', 'х'],
-    ['Ъ', 'ъ'],
-    ['/', '\\'],
-  ],
-  row3: [
-    ['Ф', 'ф'],
-    ['Ы', 'ы'],
-    ['В', 'в'],
-    ['А', 'а'],
-    ['П', 'п'],
-    ['Р', 'р'],
-    ['О', 'о'],
-    ['Л', 'л'],
-    ['Д', 'д'],
-    ['Ж', 'ж'],
-    ['Э', 'э'],
-  ],
-  row4: [
-    ['Я', 'я'],
-    ['Ч', 'ч'],
-    ['С', 'с'],
-    ['М', 'м'],
-    ['И', 'и'],
-    ['Т', 'т'],
-    ['Ь', 'ь'],
-    ['Б', 'б'],
-    ['Ю', 'ю'],
-    [',', '.'],
-  ],
-};
+export default keys;
