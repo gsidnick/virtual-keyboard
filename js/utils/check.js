274998d67b45db73a45d8cf0653949bf1d4d4b94
Nick GS <gsidnick@gmail.com>
2023-04-24 19:37:32 +0300

    feat: add symbol key check

--- js/utils/check.js
+++ js/utils/check.js
@@ -1,11 +1,16 @@
+const alphabet = {
+  en: 'abcdefghijklmnopqrstuvwxyz',
+  ru: 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
+};
+
 export function isLetterKey(locale, char) {
-  const alphabet = {
-    en: 'abcdefghijklmnopqrstuvwxyz',
-    ru: 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
-  };
   return alphabet[locale].split('').includes(char);
 }
 
 export function isSpecialKey(keyShift) {
   return keyShift === null;
+}
+
+export function isSymbolKey(locale, char) {
+  return !alphabet[locale].split('').includes(char);
 }
