79659c70fbd3cdce04c8a75ae9f7fa7400cb3211
Nick GS <gsidnick@gmail.com>
2023-04-26 14:58:57 +0300

    feat: add initialize textbox events

--- js/components/TextboxComponent.js
+++ js/components/TextboxComponent.js
@@ -4,11 +4,18 @@ class TextboxComponent extends Component {
   constructor() {
     super('textarea', 'textbox__field');
     this.value = '';
+    this.initializeTextboxEvents();
   }
 
   setValue(value) {
     this.value += value;
     this.node.innerHTML = this.value;
+  }
+
+  initializeTextboxEvents() {
+    this.node.addEventListener('input', (event) => {
+      this.value = event.target.value;
+    });
   }
 }
 
